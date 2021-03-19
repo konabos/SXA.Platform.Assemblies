5c84e51d76c7d02dd6de4219ab0906855cbe3cc8
Kamruz Jaman <kj@konabos.com>
2021-03-18 23:55:14 -0400

    Initial commit

---
+++ Generate-AssemblyList.ps1
@@ -0,0 +1,29 @@
+﻿#$files = Get-ChildItem * -Include *.dll | % { $_.VersionInfo } | Select OriginalFilename, FileVersion, ProductVersionRaw, ProductVersion
+#$files > sxa-list.txt
+
+#Get-ChildItem * -Include *.dll | % { $_.VersionInfo } | Select OriginalFilename, FileVersion, ProductVersionRaw, ProductVersion | Export-Csv $outputName -NoTypeInformation
+
+
+[CmdletBinding()]
+Param (
+    [string]
+    $SourceFolder = ".\",
+    
+    [string]
+    $TargetFile = "SXA.Platform.Assemblies.csv"
+)
+
+
+Write-Host "Generating Assembly list to $TargetFile ..." -ForegroundColor Yellow
+
+
+Get-ChildItem -Path $SourceFolder -Recurse -Include *.dll | % { $_.VersionInfo } | `
+  Select-Object -Property @{N='Filename';   E={$_.OriginalFilename}}, `
+                          @{N='Version';    E={$_.FileVersion}}, `
+                          @{N='FileVersion';E={$_.ProductVersionRaw}}, `
+                          @{N='InfoVersion';E={$_.ProductVersion}} | ` 
+  Export-Csv $TargetFile -NoTypeInformation
+
+"sep=,`n" + (Get-Content -Path $TargetFile -Raw).replace('"','') | Set-Content -Path $TargetFile
+
+Write-Host "Done!" -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SXA.Platform.Assemblies
-Sitecore Experience Accelerator Platform Assemblies lists and Powershell script to generate your own.
+Sitecore Experience Accelerator Platform Assemblies lists and Powershell script to generate your own. For use with [Helix Publising Pipeline](https://github.com/richardszalay/helix-publishing-pipeline). 
+
+You can read more here: https://www.konabos.com/blog/sxa-platform-assembly-lists
